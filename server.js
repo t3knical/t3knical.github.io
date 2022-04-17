cf9522b94f4177e8bfdd53b21784e58e2a252df8
t3knical <t3chnicald3ath@gmail.com>
2022-04-16 23:51:12 -0400

    Hourly backup

--- server.js
+++ server.js
@@ -11,52 +11,52 @@ const PORT = 443
 
 //TODO: compress changes
 const WIDTH = 2000, HEIGHT = 2000, PALETTE_SIZE = 32, COOLDOWN = 10e3 //5mins
-try {
-    BOARD = await fs.readFile('./place')
-    CHANGES = new Uint8Array(WIDTH * HEIGHT).fill(255)
-} catch (e) {
-    BOARD = new Uint8Array(WIDTH * HEIGHT)
-    CHANGES = new Uint8Array(WIDTH * HEIGHT).fill(255)
+try{
+	BOARD = await fs.readFile('./place')
+	CHANGES = new Uint8Array(WIDTH * HEIGHT).fill(255)
+}catch(e){
+	BOARD = new Uint8Array(WIDTH * HEIGHT)
+	CHANGES = new Uint8Array(WIDTH * HEIGHT).fill(255)
 }
 let newPos = [], newCols = []
 let wss, cooldowns = new Map()
 
-function runLengthChanges() {
-    //compress CHANGES with run-length encoding
-    let i = 0
-    let bufs = [Buffer.alloc(256)], blast = 0, bi = 0
-    bufs[0][bi++] = 2
-    let add = a => { bufs[blast][bi++] = a; bi == 256 && (bi = 0, bufs.push(Buffer.alloc(256)), blast++) }
-    while (true) {
-        let c = 0
-        while (CHANGES[i] == 255) c++, i++
-        if (i == CHANGES.length) break
-        //c is # of blank cells
-        //we will borrow 2 bits to store the blank cell count
-        //00 = no gap
-        //01 = 1-byte (Gaps up to 255)
-        //10 = 2-byte	(Gaps up to 65535)
-        //11 = 4-byte (idk probs never used)
-        if (c < 256) {
-            if (!c) add(CHANGES[i++])
-            else {
-                add(CHANGES[i++] + 64)
-                add(c)
-            }
-        } else if (c < 65536) {
-            add(CHANGES[i++] + 128)
-            add(c >> 8)
-            add(c)
-        } else {
-            add(CHANGES[i++] + 192)
-            add(c >> 24)
-            add(c >> 16)
-            add(c >> 8)
-            add(c)
-        }
-    }
-    bufs[blast] = bufs[blast].slice(0, bi)
-    return Buffer.concat(bufs)
+function runLengthChanges(){
+	//compress CHANGES with run-length encoding
+	let i = 0
+	let bufs = [Buffer.alloc(256)], blast = 0, bi = 0
+	bufs[0][bi++] = 2
+	let add = a => {bufs[blast][bi++]=a;bi==256&&(bi=0,bufs.push(Buffer.alloc(256)),blast++)}
+	while(true){
+		let c = 0
+		while(CHANGES[i] == 255)c++,i++
+		if(i == CHANGES.length)break
+		//c is # of blank cells
+		//we will borrow 2 bits to store the blank cell count
+		//00 = no gap
+		//01 = 1-byte (Gaps up to 255)
+		//10 = 2-byte	(Gaps up to 65535)
+		//11 = 4-byte (idk probs never used)
+		if(c < 256){
+			if(!c)add(CHANGES[i++])
+			else{
+				add(CHANGES[i++] + 64)
+				add(c)
+			}
+		}else if(c < 65536){
+			add(CHANGES[i++] + 128)
+			add(c >> 8)
+			add(c)
+		}else{
+			add(CHANGES[i++] + 192)
+			add(c >> 24)
+			add(c >> 16)
+			add(c >> 8)
+			add(c)
+		}
+	}
+	bufs[blast] = bufs[blast].slice(0,bi)
+	return Buffer.concat(bufs)
 }
 
 if (SECURE) {
@@ -85,65 +85,63 @@ let OVERRIDES
 try { VIP = new Set((await fs.readFile('vip.txt')).toString().split('\n')) } catch (e) { }
 let BANS = new Set(await fs.readFile('blacklist.txt').toString().split('\n'))
 try { OVERRIDES = new Set((await fs.readFile('cooldown_overrides.txt')).toString().split('\n')) } catch (e) { }
-wss.on('connection', async function (p, { headers, url: uri }) {
-    let url = uri.slice(1)
-    let IP = /*p._socket.remoteAddress */url || headers['x-forwarded-for']
-	console.log("A connection was made from: " + IP)
-    if (url && !VIP.has(sha256(IP))) return p.close()
-    let CD = url ? (IP.startsWith('!') ? 0 : COOLDOWN / 2) : COOLDOWN
-    if (!IP) return p.close()
-    p.lchat = 0
-    let buf = Buffer.alloc(5)
-    buf[0] = 1
-    buf.writeInt32BE(Math.ceil(cooldowns.get(IP) / 1000) || 1, 1)
-    p.send(buf)
-    players++
-    p.send(runLengthChanges())
-    p.on("error", function (data) {
-		console.log("error: ", data) 
-	})
-    p.on('message', function (data) {
-		//console.log("message: " + data)
-        if (data[0] == 15) {
-            if (p.lchat + 2500 > NOW || data.length > 400) return
-            p.lchat = NOW
-            for (let c of wss.clients) {
-                c.send(data)
-            }
-            return
-        }
-        if (data.length < 6) return //bad packet
-        let i = data.readInt32BE(1), c = data[5]
-        if (i >= BOARD.length || c >= PALETTE_SIZE) return //bad packet
-        let cd = cooldowns.get(IP)
-        if (cd > NOW) {
-            //reject
-            let data = Buffer.alloc(10)
-            data[0] = 7
-            data.writeInt32BE(Math.ceil(cd / 1000) || 1, 1)
-            data.writeInt32BE(i, 5)
-            data[9] = CHANGES[i] == 255 ? BOARD[i] : CHANGES[i]
-            p.send(data)
-            return
-        }
-        //accept
-        CHANGES[i] = c
+wss.on('connection', async function(p, {headers, url: uri}) {
+	let url = uri.slice(1)
+	let IP = /*p._socket.remoteAddress */url || headers['x-forwarded-for']
+	if(url && !VIP.has(sha256(IP)))return p.close()
+	let CD = url ? (IP.startsWith('!') ? 0 : COOLDOWN / 2) : COOLDOWN
+	if(!IP)return p.close()
+	p.lchat = 0
+	let buf = Buffer.alloc(5)
+	buf[0] = 1
+	buf.writeInt32BE(Math.ceil(cooldowns.get(IP) / 1000) || 1, 1)
+	p.send(buf)
+	players++
+	p.send(runLengthChanges())
+  p.on("error", _=>_)
+  p.on('message', async function(data) {
+		if(data[0] == 15){
+			if(p.lchat + 2500 > NOW || data.length > 400)return
+			p.lchat = NOW
+			for(let c of wss.clients){
+                		c.send(data)
+        		}
 
-        if (OVERRIDES.has(sha256(IP))) {
-            cooldowns.set(IP, NOW)
-			//console.log("cooldown for IP: " + IP + " set to: " 0)
-        }
-        else if (VIP.has(sha256(IP))) {
-            cooldowns.set(IP, NOW + (CD / 2) - 1000)
-			//console.log("cooldown for IP: " + IP + " set to: " + (NOW + (CD / 2) - 1000))
-        }
-        else {
-            cooldowns.set(IP, NOW + CD - 1000)
-        }
-        newPos.push(i)
-        newCols.push(c)
-    })
-    p.on('close', function () { players-- })
+			let txt = censor(decoder.decode(new Uint8Array(data.buffer).slice(1))).replace(/&/g,"&amp;").replace(/</g,"&lt;").replace(/"/g,"&quot;");
+			let name; [txt, name] = txt.split("\n")
+			if(name)name = name.replace(/\W+/g,'').toLowerCase()
+			if (!txt) return
+			let extraLinks = ["𝚍𝚒𝚜𝚌𝚘𝚛𝚍.𝚐𝚐", "𝐝𝐢𝐬𝐜𝐨𝐫𝐝.𝐠𝐠", "discord.gg"]
+	    		extraLinks.forEach(link => {
+                		if (txt.includes(link)) return;
+            		})
+
+	    		let msgHook = { "username": `${name || "anon"} @rplace.t3knical.com`, "content": txt }
+			if (msgHook.content.includes("@") || msgHook.content.includes("<@") || msgHook.content.includes("http")) return
+            		await fetch(WEBHOOK_URL + "?wait=true", {"method":"POST", "headers": {"content-type": "application/json"}, "body": JSON.stringify(msgHook)})
+			return;
+		}
+		if(data.length < 6)return //bad packet
+		let i = data.readInt32BE(1), c = data[5]
+		if(i >= BOARD.length || c >= PALETTE_SIZE)return //bad packet
+    let cd = cooldowns.get(IP)
+		if(cd > NOW){
+			//reject
+			let data = Buffer.alloc(10)
+			data[0] = 7
+			data.writeInt32BE(Math.ceil(cd / 1000) || 1, 1)
+			data.writeInt32BE(i, 5)
+			data[9] = CHANGES[i] == 255 ? BOARD[i] : CHANGES[i]
+			p.send(data)
+			return
+		}
+		//accept
+		CHANGES[i] = c
+		cooldowns.set(IP, NOW + CD - 1000)
+		newPos.push(i)
+		newCols.push(c)
+  })
+	p.on('close', function(){ players-- })
 })
 let NOW = Date.now()
 setInterval(() => {
@@ -166,15 +164,15 @@ async function pushUpdatesToGitHub() {
 }
 
 async function pushImage() {   
-	pushUpdatesToGitHub()
+	await pushUpdatesToGitHub()
 	//await new Promise((resolve, reject) => exec('git add *;git commit -a -m "Hourly backup";git push --force ' + ORIGIN + '/t3knical/t3knical.github.io', e => e ? reject(e) : resolve()))
 	
-	//serve old changes for 5 more mins just to be 100% safe
+	//serve old changes for 10 more mins just to be 100% safe
     let curr = new Uint8Array(CHANGES)
     setTimeout(() => {
         //after 5 minutes, remove all old changes. Where there is a new change, curr[i] != CHANGES[i] and so it will be kept, but otherwise, remove
         for (let i = curr.length - 1; i >= 0; i--)if (curr[i] == CHANGES[i]) CHANGES[i] = 255
-    }, 300 * 1000)
+    }, 600 * 1000)
 }
 setInterval(function () {
     if (!newPos.length) return
